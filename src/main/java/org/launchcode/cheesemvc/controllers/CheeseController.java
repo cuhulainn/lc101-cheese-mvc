9e18b8cd3e7bac4d1d9b06bb85e20fc035d69332
Connor McGinn <connor.mcginn@gmail.com>
2019-08-23 06:58:42 -0500

    class 9 prep complete - edit functionality added

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -4,10 +4,7 @@ import org.launchcode.cheesemvc.models.Cheese;
 import org.launchcode.cheesemvc.models.CheeseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -48,6 +45,20 @@ public class CheeseController {
             CheeseData.remove(cheeseId);
         }
 
+        return "redirect: ";
+    }
+
+    @RequestMapping(value="edit/{cheeseId}", method = RequestMethod.GET)
+    public String displayEditForm(Model model, @PathVariable int cheeseId) {
+        model.addAttribute("cheese", CheeseData.getById(cheeseId));
+        return "cheese/edit";
+    }
+
+    @RequestMapping(value="edit", method = RequestMethod.POST)
+    public String processEditForm(int cheeseId, String name, String description) {
+        Cheese cheeseToEdit = CheeseData.getById(cheeseId);
+        cheeseToEdit.setName(name);
+        cheeseToEdit.setDescription(description);
         return "redirect: ";
     }
 }
