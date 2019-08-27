20a37106210ec43d50e92b8693ce9492713ea3e3
Connor McGinn <connor.mcginn@gmail.com>
2019-08-27 08:20:30 -0500

    set up initial connection to DB with Dao

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,23 +1,26 @@
 package org.launchcode.cheesemvc.controllers;
 
 import org.launchcode.cheesemvc.models.Cheese;
-import org.launchcode.cheesemvc.models.CheeseData;
 import org.launchcode.cheesemvc.models.CheeseType;
+import org.launchcode.cheesemvc.models.data.CheeseDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
+    @Autowired
+    private CheeseDao cheeseDao;
+
     @RequestMapping(value="")
     public String index(Model model) {
-        model.addAttribute("cheeses", CheeseData.getAll());
+        model.addAttribute("cheeses", cheeseDao.findAll());
         model.addAttribute("title", "Cheesy Cheeses");
         return "cheese/index";
     }
@@ -38,14 +41,14 @@ public class CheeseController {
             return "cheese/add";
         }
 
-        CheeseData.add(newCheese);
+        cheeseDao.save(newCheese);
         return "redirect: ";
     }
 
     @RequestMapping(value="remove", method = RequestMethod.GET)
     public String displayRemoveCheeseForm(Model model) {
         model.addAttribute( "title", "Remove Cheese");
-        model.addAttribute("cheeses", CheeseData.getAll());
+        model.addAttribute("cheeses", cheeseDao.findAll());
         return "cheese/remove";
     }
 
@@ -53,7 +56,7 @@ public class CheeseController {
     public String processRemoveCheeseForm(@RequestParam int[] cheeseIds){
 
         for (int cheeseId : cheeseIds) {
-            CheeseData.remove(cheeseId);
+            cheeseDao.deleteById(cheeseId);
         }
 
         return "redirect: ";
@@ -61,25 +64,25 @@ public class CheeseController {
 
     @RequestMapping(value="edit/{cheeseId}", method = RequestMethod.GET)
     public String displayEditForm(Model model, @PathVariable int cheeseId) {
-        model.addAttribute("cheese", CheeseData.getById(cheeseId));
+        model.addAttribute("cheese", cheeseDao.findById(cheeseId));
         model.addAttribute("cheeseTypes", CheeseType.values());
         return "cheese/edit";
     }
 
-    @RequestMapping(value="edit", method = RequestMethod.POST)
+    /*@RequestMapping(value="edit", method = RequestMethod.POST)
     public String processEditForm(@ModelAttribute @Valid Cheese editedCheese, Errors errors, Model model) {
 
         if (errors.hasErrors()) {
-            model.addAttribute("cheese", CheeseData.getById(editedCheese.getCheeseId()));
+            model.addAttribute("cheese", cheeseDao.findById(editedCheese.getId()));
             model.addAttribute("cheeseTypes", CheeseType.values());
             return "cheese/edit";
         }
 
-        Cheese cheeseToEdit = CheeseData.getById(editedCheese.getCheeseId());
+        Cheese cheeseToEdit = cheeseDao.findById(editedCheese.getId());
         cheeseToEdit.setName(editedCheese.getName());
         cheeseToEdit.setDescription(editedCheese.getDescription());
         cheeseToEdit.setType(editedCheese.getType());
         cheeseToEdit.setCheeseRating(editedCheese.getCheeseRating());
         return "redirect: ";
-    }
+    }*/
 }
--- src/main/java/org/launchcode/cheesemvc/models/Cheese.java
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -1,11 +1,19 @@
 package org.launchcode.cheesemvc.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Cheese {
+
+    @Id
+    @GeneratedValue
+    private int id;
 
     @NotNull
     @Size(min=3, max=15)
@@ -15,7 +23,6 @@ public class Cheese {
     @Size(min=1, message = "Description must not be blank")
     private String description;
 
-
     private CheeseType type;
 
     @NotNull
@@ -23,26 +30,15 @@ public class Cheese {
     @Max(5)
     private int cheeseRating;
 
-    private int cheeseId;
-    private static int nextId = 1;
-
     public Cheese(String name, String description) {
-        this();
         this.name = name;
         this.description = description;
     }
 
-    public Cheese() {
-        cheeseId = nextId;
-        nextId++;
-    }
+    public Cheese() { }
 
-    public int getCheeseId() {
-        return cheeseId;
-    }
-
-    public void setCheeseId(int cheeseId) {
-        this.cheeseId = cheeseId;
+    public int getId() {
+        return id;
     }
 
     public String getName() {
